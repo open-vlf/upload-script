78945a081a19298e90e2a555ef72819e22f110ab
lsglucas <lsglucas@protonmail.com>
2023-03-30 17:26:25 -0300

    Add firestore db

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import boto3
 from fnmatch import fnmatch
 from rich import print
 from rich.progress import track
+from datetime import datetime
 
+from firebase_admin import firestore
 from firebase_admin import credentials
 
 root = "./"
@@ -21,6 +23,8 @@ s3 = boto3.client(
     aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
 )
 
+NARROWBAND = "NARROWBAND"
+BROADBAND = "BROADBAND"
 
 narrowband_file_size = 26
 broadband_file_size = 22
@@ -30,7 +34,49 @@ narrowband_queue = []
 broadband_queue = []
 
 
-def saveJsonFile() -> None:
+def get_narrowband_data(filename: str) -> dict:
+    return {
+        "Station_ID": filename[:2],
+        "Year": filename[2:4],
+        "Month": filename[4:6],
+        "Day": filename[6:8],
+        "Hour": filename[8:10],
+        "Minute": filename[10:12],
+        "Second": filename[12:14],
+        "Transmitter": filename[14:17],
+        "_": filename[17:18],
+        "Usually not used": filename[18:19],
+        "CC": filename[19:21],
+        # CC — 00 for N/S channel:01 for E/W channel
+        "Type_ABCDF": filename[21:22],
+        # A is low resolution (1 Hz sampling rate) amplitude
+        # B is low resolution (1 Hz sampling rate) phase
+        # C is high resolution (50 Hz sampling rate) amplitude
+        # D is high resolution (50 Hz sampling rate) phase
+        # F is high resolution (50 Hz sampling rate) effective group delay
+        ".mat": filename[22:26],
+    }
+
+
+def get_broadband_data(filename: str) -> dict:
+    return {
+        "Station_ID": filename[:2],
+        "Year": filename[2:4],
+        "Month": filename[4:6],
+        "Day": filename[6:8],
+        "Hour": filename[8:10],
+        "Minute": filename[10:12],
+        "Second": filename[12:14],
+        "_": filename[14:15],
+        "A": filename[15:16],
+        # A — Sampling rate. 0 for 100 kHz sampled data (VLF), 1 for 1 MHz sampled data (LF), 2 for 25 kHz sampled data (Siple station experiment).
+        "CC": filename[16:18],
+        # CC — 00 for N/S channel, 01 for E/W channel
+        ".mat": filename[18:22],
+    }
+
+
+def save_json_data() -> None:
     with open(db_file_name, "w+") as outfile:
         json.dump(
             {
@@ -42,7 +88,7 @@ def saveJsonFile() -> None:
         )
 
 
-def getJsonData() -> dict:
+def get_json_data() -> dict:
     print("Loading storage data...")
 
     try:
@@ -64,50 +110,115 @@ def getJsonData() -> dict:
         print("No storage data found")
 
 
-def shouldProcessFile(path: str) -> bool:
+def should_process_file(path: str) -> bool:
     return not (
         path in uploaded_files or path in narrowband_queue or path in broadband_queue
     )
 
 
-def uploadToS3(path: str, type: str) -> None:
+def store_narrowband(file_name: str, path: str, db) -> None:
+    file_data = get_narrowband_data(file_name)
+
+    data = f'20{file_data["Year"]}/{file_data["Month"]}/{file_data["Day"]}'
+    collection_name = f'{data}/narrowband/{file_data["Station_ID"]}'
+
+    s3.upload_file(collection_name, bucket_name, file_name)
+
+    db.collection(collection_name).document(file_name).set(
+        {
+            "fileName": file_name,
+            "path": path,
+            "url": f"https://{bucket_name}.s3.sa-east-1.amazonaws.com/{collection_name}/{file_name}",
+            "stationId": file_data["Station_ID"],
+            "transmitter": file_data["Transmitter"],
+            "dateTime": datetime(
+                int(f"20{file_data['Year']}"),
+                int(file_data["Month"]),
+                int(file_data["Day"]),
+                int(file_data["Hour"]),
+                int(file_data["Minute"]),
+                int(file_data["Second"]),
+            ),
+            "CC": file_data["CC"],
+            "typeABCDF": file_data["Type_ABCDF"],
+            "timestamp": firestore.SERVER_TIMESTAMP,
+            "endpointType": "AWS S3",
+        }
+    )
+
+
+def store_broadband(file_name: str, path: str, db) -> None:
+    file_data = get_narrowband_data(file_name)
+
+    data = f'20{file_data["Year"]}/{file_data["Month"]}/{file_data["Day"]}'
+    collection_name = f'{data}/broadband/{file_data["Station_ID"]}'
+
+    s3.upload_file(collection_name, bucket_name, file_name)
+
+    db.collection(collection_name).document(file_name).set(
+        {
+            "fileName": file_name,
+            "path": path,
+            "url": f"https://{bucket_name}.s3.sa-east-1.amazonaws.com/{collection_name}/{file_name}",
+            "stationId": file_data["Station_ID"],
+            "dateTime": datetime(
+                int(f"20{file_data['Year']}"),
+                int(file_data["Month"]),
+                int(file_data["Day"]),
+                int(file_data["Hour"]),
+                int(file_data["Minute"]),
+                int(file_data["Second"]),
+            ),
+            "CC": file_data["CC"],
+            "A": file_data["A"],
+            "timestamp": firestore.SERVER_TIMESTAMP,
+            "endpointType": "AWS S3",
+        }
+    )
+
+
+def upload_to_s3(path: str, type: str, db) -> None:
     file_name = os.path.basename(path)
 
-    s3.upload_file(path, bucket_name, file_name)
-
-    if type == "NARROWBAND":
+    if type == NARROWBAND:
+        store_narrowband(file_name, path, db)
         narrowband_queue.remove(path)
-    elif type == "BROADBAND":
+
+    elif type == BROADBAND:
+        store_broadband(file_name, path, db)
         broadband_queue.remove(path)
 
     uploaded_files.append(path)
 
-    saveJsonFile()
+    save_json_data()
 
 
 def main() -> None:
     cred = credentials.Certificate("./credentials.json")
     firebase_admin.initialize_app(cred)
+    db = firestore.client()
 
-    getJsonData()
+    get_json_data()
 
     for path, subdirs, files in track(os.walk(root), description="Finding .MAT files"):
         for name in files:
             if fnmatch(name, pattern):
                 file_path = os.path.join(path, name)
 
-                if len(name) == narrowband_file_size and shouldProcessFile(file_path):
+                if len(name) == narrowband_file_size and should_process_file(file_path):
                     narrowband_queue.append(file_path)
                     print(f"Found narrowband file {narrowband_queue[-1]}")
 
-                elif len(name) == broadband_file_size and shouldProcessFile(file_path):
+                elif len(name) == broadband_file_size and should_process_file(
+                    file_path
+                ):
                     broadband_queue.append(file_path)
                     print(f"Found broadband file {broadband_queue[-1]}")
 
                 elif (
                     len(name) == narrowband_file_size
                     or len(name) == broadband_file_size
-                ) and not shouldProcessFile(file_path):
+                ) and not should_process_file(file_path):
                     print(f"File {file_path} is already uploaded or in queue")
 
                 else:
@@ -118,12 +229,12 @@ def main() -> None:
     )
 
     for item in track(narrowband_queue, description="Working on narrowband files"):
-        uploadToS3(item, "NARROWBAND")
+        upload_to_s3(item, NARROWBAND, db)
 
     for item in track(broadband_queue, description="Working on broadband files"):
-        uploadToS3(item, "BROADBAND")
+        upload_to_s3(item, BROADBAND, db)
 
-    saveJsonFile()
+    save_json_data()
 
     print("Program finished.")
 
